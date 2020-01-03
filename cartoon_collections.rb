ce84ebe12785f423221ef441fd96315789b82982
Reese M Bowman <reesemb1@gmail.com>
2020-01-03 22:36:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,10 @@ def long_planeteer_calls(ary)
   ary.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(potential_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  potential_cheese.find do |thing|
+    if thing ==
 end
